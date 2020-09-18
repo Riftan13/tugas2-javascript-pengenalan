b84dfded21e76579ebe87e2cfc59a4737319bc26
Riftan13 <55617983+Riftan13@users.noreply.github.com>
2020-09-18 10:49:22 +0700

    Add files via upload

---
+++ tugas2.js
@@ -0,0 +1,11 @@
+var nip = 1212
+var nama = "riftan"
+var kerja = true
+var duit = null
+var hobi = ["futsal", "bersepeda", "main game"]
+
+console.log (nip)
+console.log (nama)
+console.log (kerja)
+console.log (duit)
+console.log (hobi [1], hobi[2])
